2347da488fb1d2c579a77580045ce8f2387d3928
anand26072000 <mail2kaanand@gmail.com>
2025-12-03 11:59:07 +0530

    Pwa images added

--- vite.config.ts
+++ vite.config.ts
@@ -34,19 +34,19 @@ export default defineConfig(({ mode }) => {
           orientation: 'portrait-primary',
           icons: [
             {
-              src: '/pwa-192x192.png',
+              src: '/android-launchericon-192-192.png',
               sizes: '192x192',
               type: 'image/png',
               purpose: 'any'
             },
             {
-              src: '/pwa-512x512.png',
+              src: '/android-launchericon-512-512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any'
             },
             {
-              src: '/pwa-maskable-512x512.png',
+              src: '/android-launchericon-512-512.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'maskable'
